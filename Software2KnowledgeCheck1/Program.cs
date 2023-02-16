c3db31500e1662f88d0e197077ac7770bd13a989
ChefBoyaRX <oodel@Devons-Mac-mini.lan>
2023-02-16 13:04:00 -0500

    lets see

---
+++ Software2KnowledgeCheck1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    public class entryppoint
+    {
+        static void Main()
+        {
+
+        }
+
+    }
+}
